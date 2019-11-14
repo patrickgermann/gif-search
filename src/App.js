cb96290e2f686545404614c40277739da6a3a6c8
Patrick Germann <p.germann@me.com>
2019-11-14 21:23:55 +0100

    Add searchForm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 import SearchForm from './Components/SearchForm';
 import GifList from './Components/GifList';
 
@@ -13,10 +14,15 @@ export default class App extends Component {
   } 
 
   componentDidMount() {
-    fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
-      .then(response => response.json())
-      .then(responseData => {
-        this.setState({gifs: responseData.data });
+    //this.performSearch();
+  }
+
+  performSearch = (query) => {
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
+      .then(response => {
+        this.setState({
+          gifs: response.data.data
+        });
       })
       .catch(error => {
         console.log('Error fetching and parsing data', error);
@@ -30,11 +36,11 @@ export default class App extends Component {
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm />      
+            <SearchForm onSearch={this.performSearch} />      
           </div>   
         </div>    
         <div className="main-content">
-          <GifList />
+          <GifList data={this.state.gifs} />
         </div>
       </div>
     );
